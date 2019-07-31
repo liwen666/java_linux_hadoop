31ec137e15d85d0568fa74ba12acb338f534d760
liwen <1438131288@qq.com>
2019-07-31 21:36:15 +0800

    nginx的配置实现反向代理

--- linux/src/main/java/com/java/hadoop/fdfs/FastFdfsSite.java
+++ linux/src/main/java/com/java/hadoop/fdfs/FastFdfsSite.java
@@ -10,6 +10,7 @@ public class FastFdfsSite {
 
         //从服务器配置
 //        配置获取模板配置文件
+//        fileManager.getLinuxFileCategory("192.168.42.230","root","root",22,"lwnginx.conf","/etc/fdfs/");
 //        fileManager.getLinuxFileCategory("192.168.42.230","root","root",22,"lwnginx.conf","/usr/local/nginx/conf/");
 //        fileManager.getLinuxFileCategory("192.168.42.230","root","root",22,"nginx.conf","/usr/local/nginx/conf/");
 //        fileManager.getLinuxFileCategory("192.168.42.230","root","root",22,"tracker.conf","/etc/fdfs/");
@@ -21,7 +22,8 @@ public class FastFdfsSite {
 
 
 
-        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"lwnginx.conf");
+        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"nginx.conf");
+//        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"lwnginx.conf");
 //        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"config");
 //        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"storage.conf");
 //        fileManager.upLinuxFileCategory("192.168.42.230","root","root",22,"tracker.conf");
