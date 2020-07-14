e66dd558629e73d946316535f8068fc9b10ae4b2
liwen <1438131288@qqcom>
2020-07-14 13:41:01 +0800

    commit nacos

---
+++ linux/src/main/java/com/java/hadoop/nacos/Nacos.java
@@ -0,0 +1,42 @@
+package com.java.hadoop.nacos;
+
+import com.java.hadoop.filemanager.FileManager;
+
+/**
+ * <p>
+ * 描述
+ * </p>
+ *
+ * @author lw
+ * @since 2019/5/26 23:40
+ */
+public class Nacos {
+
+
+    public static void main(String[] args) throws Exception {
+        String filePath="/home/liwen/nacos/nacos/conf";
+        FileManager fileManager = new FileManager();
+
+
+
+        //从服务器配置
+        fileManager.getLinuxFileCategory("nacos", "192.168.42.220","root","root",22,"schema.sql",filePath);
+        fileManager.getLinuxFileCategory("nacos", "192.168.42.220","root","root",22,"nacos-mysql.sql",filePath);
+//        fileManager.getLinuxFileCategory("172.16.102.22","jrxany","20191022",22,"application.yml",filePath);
+
+
+
+
+//        fileManager.upLinuxFileCategory("172.16.102.22","jrxany","20191022",22,"application-local_l.yml");
+//
+        //配置端口
+//        FireWallController fwc = new FireWallController();
+//        fwc.modifyPortSetByParam("root","192.168.42.220",22,"root");
+//        fwc.execShellByParam("root","192.168.42.220",22,"root");
+//        FireWallController fwc2 = new FireWallController();
+//        fwc2.modifyPortSetByParam("root","192.168.42.220",22,"root");
+//        fwc2.execShellByParam("root","192.168.42.220",22,"root");
+
+    }
+
+}
---
+++ linux/src/main/java/com/java/hadoop/shell_sit/testadd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 1111111
+exit 1
